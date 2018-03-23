4e0854b74e443f48ad8c0bd418367a7c6bbb5966
Steven <steven.james.correia@gmail.com>
2018-03-23 14:00:00 +0100

    Adds Category and Item classes

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -15,6 +15,44 @@ class User(Base):
     picture = Column(String(250))
 
 
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class CatalogItem(Base):
+    __tablename__ = 'catalog_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
+
+
 engine = create_engine('sqlite:///catalog.db')
 
 
