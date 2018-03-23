62c2927e0103f97db394a11ab437221b6381db25
Steven <steven.james.correia@gmail.com>
2018-03-23 18:01:31 +0100

    Adds comments and structure for site routing

--- vagrant/catalog/views.py
+++ vagrant/catalog/views.py
@@ -1,8 +1,9 @@
-from models import Base, User
+from models import Base, User, Category, CatalogItem
 from flask import Flask, jsonify, request, url_for, abort, g, render_template
+from flask import session as login_session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, asc
 
 from flask_httpauth import HTTPBasicAuth
 import json
@@ -24,10 +25,40 @@ session = DBSession()
 app = Flask(__name__)
 
 
+# Login
+
+# Logout
+
+
 @app.route('/')
 @app.route('/catalog')
-def get_resource():
-    return "Hello catalog!"
+def showCatalog():
+    categories = session.query(Category).order_by(asc(Category.name))
+    return jsonify(categories=[c.serialize for c in categories])
+
+
+@app.route('/catalog/JSON')
+def categoryJSON():
+    categories = session.query(Category).all()
+    return jsonify(categories=[c.serialize for c in categories])
+
+
+# Add a Catalog Category
+
+# Edit a Catalog Category
+
+# Delete a Catalog Category
+
+# Show a Catalog Category
+@app.route('/catalog/<string:category>')
+def showItemsInCategory(category):
+    return "Items in category: %s" % category
+
+# Add a Catalog Item
+
+# Edit a Catalog Item
+
+# Delete a Catalog Item
 
 
 if __name__ == '__main__':
