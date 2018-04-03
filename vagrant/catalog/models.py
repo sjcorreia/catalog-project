f2b4583181434715d5df9f6f2fff1b74da5601d6
Steven <steven.james.correia@gmail.com>
2018-04-03 20:17:24 +0200

    Increases buffer size for description

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -37,7 +37,7 @@ class CatalogItem(Base):
 
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
-    description = Column(String(250))
+    description = Column(String(1000))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
     user_id = Column(Integer, ForeignKey('user.id'))
