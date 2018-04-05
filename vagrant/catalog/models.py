de366e20435ec32fb1de8e05fc576631ff5f9be1
Steven <steven.james.correia@gmail.com>
2018-04-05 11:22:07 +0200

    Adds password field and functions for verification

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -2,18 +2,50 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+import random
+import string
+from itsdangerous import(TimedJSONWebSignatureSerializer as
+                         Serializer, BadSignature, SignatureExpired)
+
+Base = declarative_base()
+secret_key = ''.join(random.choice(
+    string.ascii_uppercase + string.digits) for x in range(32))
 
 Base = declarative_base()
 
 
 class User(Base):
     __tablename__ = 'user'
-
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    picture = Column(String(250))
+    username = Column(String(32), index=True)
+    picture = Column(String)
+    email = Column(String)
+    password_hash = Column(String(64))
 
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+    	s = Serializer(secret_key, expires_in = expiration)
+    	return s.dumps({'id': self.id })
+
+    @staticmethod
+    def verify_auth_token(token):
+    	s = Serializer(secret_key)
+    	try:
+    		data = s.loads(token)
+    	except SignatureExpired:
+    		#Valid Token, but expired
+    		return None
+    	except BadSignature:
+    		#Invalid Token
+    		return None
+    	user_id = data['id']
+    	return user_id
 
 class Category(Base):
     __tablename__ = 'category'
