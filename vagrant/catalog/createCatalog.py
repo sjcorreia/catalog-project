4fd988e8a754c9d648825d63e8629cc3e020d759
Steven <steven.james.correia@gmail.com>
2018-04-05 14:39:24 +0200

    Changes dummy username, adds password

--- vagrant/catalog/createCatalog.py
+++ vagrant/catalog/createCatalog.py
@@ -22,8 +22,9 @@ session = DBSession()
 
 
 # Create dummy user
-User1 = User(username="Robo Barista", email="tinnyTim@udacity.com",
+User1 = User(username="RoboUser", email="tinnyTim@udacity.com",
              picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+User1.hash_password("Udacity1")
 session.add(User1)
 session.commit()
 
