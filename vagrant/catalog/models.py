4dc379f47c4b176d0598b8d388a665a0f59aa474
Steven <steven.james.correia@gmail.com>
2018-04-06 14:12:13 +0200

    Code clean up accorinding to PEP8

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -5,8 +5,8 @@ from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
 import random
 import string
-from itsdangerous import(TimedJSONWebSignatureSerializer as
-                         Serializer, BadSignature, SignatureExpired)
+from itsdangerous import (TimedJSONWebSignatureSerializer as
+                          Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
 secret_key = ''.join(random.choice(
@@ -30,22 +30,23 @@ class User(Base):
         return pwd_context.verify(password, self.password_hash)
 
     def generate_auth_token(self, expiration=600):
-    	s = Serializer(secret_key, expires_in = expiration)
-    	return s.dumps({'id': self.id })
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
 
     @staticmethod
     def verify_auth_token(token):
-    	s = Serializer(secret_key)
-    	try:
-    		data = s.loads(token)
-    	except SignatureExpired:
-    		#Valid Token, but expired
-    		return None
-    	except BadSignature:
-    		#Invalid Token
-    		return None
-    	user_id = data['id']
-    	return user_id
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            # Valid Token, but expired
+            return None
+        except BadSignature:
+            # Invalid Token
+            return None
+        user_id = data['id']
+        return user_id
+
 
 class Category(Base):
     __tablename__ = 'category'
